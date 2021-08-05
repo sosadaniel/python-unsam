142a861018bcbd0344c7cce66071280411fc9cef
DanielSosa12 <sosa.danielb@gmail.com>
2021-08-05 11:51:08 -0300

    resolviendo ejercicios

---
+++ ejercicios_python/Clase01/saludos.py
@@ -0,0 +1,8 @@
+"""
+Escribí un programa llamado saludo.py que pregunte el nombre de el usuario,
+imprima un saludo (por ejemplo, "Hola, Juana") y termine.
+
+"""
+print("¿Cual es tu nombre?")
+nombre = input("> ")
+print("Hola," , nombre)
