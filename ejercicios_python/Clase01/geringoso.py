7ab248dd206de93966c748708cbf758d836f7ef1
DanielSosa12 <sosa.danielb@gmail.com>
2021-08-17 16:13:20 -0300

    mejoras en el código

--- ejercicios_python/Clase01/geringoso.py
+++ ejercicios_python/Clase01/geringoso.py
@@ -9,15 +9,6 @@ capadepenapa = ''
 
 for c in cadena:
     capadepenapa += c
-    if c == 'a':
-        capadepenapa += 'pa'
-    elif c == 'e':
-        capadepenapa += 'pe'
-    elif c == 'i':
-        capadepenapa += 'pi'
-    elif c == 'o':
-        capadepenapa += 'po'
-    elif c == 'u':
-        capadepenapa += 'pu'
-
+    if c in 'aeiou':
+        capadepenapa += 'p'+ c
 print(capadepenapa)
---
+++ ejercicios_python/Clase01/prueba.py
@@ -0,0 +1,7 @@
+
+palabra = "palabra"
+lista = [10, 12, 66, 457]
+
+
+for numero in range(0,10):
+    print("Voy por el número: ", numero)
