3042a50251e027d6f063987e2eeabd84d1f37253
DanielSosa12 <sosa.danielb@gmail.com>
2021-08-18 00:57:59 -0300

    resolviendo ejercicios

---
+++ ejercicios_python/Clase02/diccionario_geringoso.py
@@ -0,0 +1,21 @@
+"""
+Ejercicio 2.14: Diccionario geringoso
+    Construí una función que, a partir de una lista de palabras, devuelva un diccionario geringoso. Las claves del diccionario deben ser las 
+    palabras de la lista y los valores deben ser sus traducciones al geringoso (como en el Ejercicio 1.18). Probá tu función para la lista
+    ['banana', 'manzana', 'mandarina'].
+
+"""
+
+lista = ['banana', 'manzana', 'mandarina']
+diccionario = {}
+capadepenapa = ''
+
+for elemento in lista:
+    for c in elemento:
+        capadepenapa += c
+        if c in 'aeiou':
+            capadepenapa += 'p'+ c
+    diccionario[elemento] = capadepenapa
+    capadepenapa = ""
+    
+print(diccionario)
